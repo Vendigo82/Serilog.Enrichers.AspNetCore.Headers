aa0a233a759cc303c03b8dbe9dadd1ebad420453
Vendigo82 <Vendigo82@yandex.ru>
2021-05-03 10:10:26 +0800

    Добавление Header-ов в логку

---
+++ src/Serilog.Enrichers.AspNetCore.Headers/HeaderEnricher.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Serilog.Core;
+using Serilog.Enrichers.AspNetCore.Headers;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serilog.Enrichers.AspNetCore.Headers
+{
+    public class HeaderEnricher : ILogEventEnricher
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly string _header;
+        private readonly string _propertyName;
+
+        public HeaderEnricher(IHttpContextAccessor contextAccessor, string header, string propertyName = null)
+        {
+            _contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
+            _header = header ?? throw new ArgumentNullException(nameof(header));
+            _propertyName = propertyName ?? _header;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (_contextAccessor.HttpContext == null)
+                return;
+
+            var value = _contextAccessor.HttpContext.Request.Headers[_header].FirstOrDefault();
+            logEvent.AddOrUpdateProperty(new LogEventProperty(_propertyName, new ScalarValue(value)));
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.AspNetCore.Headers/LoggerConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog.Configuration;
+using Serilog.Enrichers.AspNetCore.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Enrichers
+{
+    public static class LoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration WithHeader(this LoggerEnrichmentConfiguration enrichmentConfiguration, IServiceProvider sp, string headerName, string propertyName = null)
+        {
+            if (enrichmentConfiguration == null) 
+                throw new ArgumentNullException(nameof(enrichmentConfiguration));
+
+
+            return enrichmentConfiguration.With(ActivatorUtilities.CreateInstance<HeaderEnricher>(sp, headerName, propertyName));
+        }
+
+        public static LoggerConfiguration WithCorrelationId(this LoggerEnrichmentConfiguration enrichmentConfiguration, IServiceProvider sp, 
+            string headerName = "X-Correlation-ID",
+            string propertyName = "CorrelationId")
+            => WithHeader(enrichmentConfiguration, sp, headerName, propertyName);
+    }
+}
